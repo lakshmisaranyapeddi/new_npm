b32ec9ecbbfc53915291c58a2d01b8cac03f97f5
saranyapeddi <saranya.peddi@tcs.com>
2018-07-24 12:15:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+# new_npm
+
+//<Description of what the package does>
+
+
+it does some installations
+
+
+
+
+// ## Usage
+
+
+
+// Install the package using npm :
+
+ npm install is-null-or-empty --save 
+Then, require the package and use it:
+
+ // [Comment: To check if this usage is proper]
+
+ var isNullOrEmpty = require('is-null-or-empty');
+
+
+
+ console.log(isNullOrEmpty("")); // true
+
+
+
+ console.log(isNullOrEmpty("Hello World")); // false
